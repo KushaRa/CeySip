6c1070d8a39a67c419c1dd59966335a65cc63805
kushana <rashmisenadheera98@gmail.com>
2024-10-31 15:32:16 +0530

    create navbar

---
+++ ceylon_t/src/Components/navBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div>
+        <header>
+            <h3>CeySip</h3>
+
+            <nav>
+                <a href='/#'>Home</a>
+                <a href='/#'>Home</a>
+                <a href='/#'>Home</a>
+            </nav>
+
+            <h3>
+                Account
+            </h3>
+        </header>
+      
+    </div>
+  );
+};
+
+export default NavBar;
