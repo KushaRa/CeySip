75c3c1ecbcb4e0d378f645de2b9777c8b173dfb8
kushana <rashmisenadheera98@gmail.com>
2024-11-15 23:16:33 +0530

    changes

---
+++ ceylon_t/src/Pages/section2/recipe.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './recipe.css';
+
+function recipe() {
+  return (
+    <div className='secHeader'>
+      <h3 className="sech3">Tea Treats</h3>
+      
+    </div>
+  )
+}
+
+export default recipe
