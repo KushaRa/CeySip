3733f1c24a815f926e35f3f1f1e73055b3484a80
kushana <rashmisenadheera98@gmail.com>
2024-11-16 10:58:58 +0530

    created basic structure

--- ceylon_t/src/App.js
+++ ceylon_t/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Hero from '../src/Components/hero';
-import Section1 from '../src/Pages/section1/section'
+import Section1 from '../src/Pages/section1/section';
+import Recipe from './Pages/section2/recipe';
 
 function App() {
   return (
     <div>
       <Hero/>
       <Section1/>
+      <Recipe/>
+      
       
     </div>
   );
--- ceylon_t/src/Pages/section2/recipe.jsx
+++ ceylon_t/src/Pages/section2/recipe.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
 import './recipe.css';
+import img1 from '../../Assets/image1.png';
+import img2 from '../../Assets/image2.png';
+import img3 from '../../Assets/image3.png';
+import './recipe.css'
 
-function recipe() {
+
+function Recipe() {
   return (
-    <div className='secHeader'>
-      <h3 className="sech3">Tea Treats</h3>
-      
+    
+    <div className='sec2'>
+      <h3 className="sech3">Sip Something New</h3>
+
+    <div className='recipeCntan'>
+      <div className='card'>
+        <img src={img1} alt='chai' />
+        <div className='textOverlay'>Delicious Chai</div>
+      </div>
+
+      <div className='card'>
+        <img src={img2} alt='chai'/>
+
+      </div>
+
+      <div className='card'>
+        <img src={img3} alt='chai'/>
+
+      </div>
+    </div>      
     </div>
+
+    
   )
 }
 
-export default recipe
+export default Recipe
