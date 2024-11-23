d3377dbbe59c77c05d9fc212ccb4c4bd37eeef49
kushana <rashmisenadheera98@gmail.com>
2024-11-23 19:23:12 +0530

    added corrsal

--- ceylon_t/src/Pages/section2/recipe.jsx
+++ ceylon_t/src/Pages/section2/recipe.jsx
@@ -3,10 +3,46 @@ import './recipe.css';
 import img1 from '../../Assets/image1.png';
 import img2 from '../../Assets/image2.png';
 import img3 from '../../Assets/image3.png';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import './recipe.css'
 
 
 function Recipe() {
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
   return (
     
     <div className='sec2'>
@@ -44,9 +80,34 @@ function Recipe() {
          butterfly pea flower tea, which changes color when mixed with 
          acidic ingredients like lemon juice. It is typically 
         served chilled, with mint leaves for garnish.
-        </p>
+        </p>      
           </div>
+      </div>
 
+      <div className='card'>
+        <img src={img3} alt='chai'/>
+        <div className='textOverlay' style={{marginLeft:'40px'}}>Butterfly Pea Tea Lemonade</div>
+        <div className='cOverlay'>
+          <p>
+        Known for its vibrant color, this drink is made from
+         butterfly pea flower tea, which changes color when mixed with 
+         acidic ingredients like lemon juice. It is typically 
+        served chilled, with mint leaves for garnish.
+        </p>      
+          </div>
+      </div>
+
+      <div className='card'>
+        <img src={img3} alt='chai'/>
+        <div className='textOverlay' style={{marginLeft:'40px'}}>Butterfly Pea Tea Lemonade</div>
+        <div className='cOverlay'>
+          <p>
+        Known for its vibrant color, this drink is made from
+         butterfly pea flower tea, which changes color when mixed with 
+         acidic ingredients like lemon juice. It is typically 
+        served chilled, with mint leaves for garnish.
+        </p>      
+          </div>
       </div>
      
     </div>  
