568e7096cbeb44680b3901f395f0a6ae1f0c0f80
kushana <rashmisenadheera98@gmail.com>
2024-12-05 23:02:10 +0530

    update:basic location update

--- ceylon_t/src/Pages/section3/location.jsx
+++ ceylon_t/src/Pages/section3/location.jsx
@@ -6,8 +6,11 @@ const Locations = () => {
     <div className='sec4'>
         <div className='secHeader'>
             <h3 className='sechhead'>Explore the Heart of Tea</h3>
-        </div>
-
+                    </div>
+          <h3 className='sechhead'>Top States</h3>
+  <div className='stateCard'>
+    <div>Map</div>
+  </div>
       
     </div>
   );
